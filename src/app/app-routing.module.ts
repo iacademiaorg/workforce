3423a03079abe25fbe33357967987b436a738de7
mudasar-sahl <mudasar@sahlbs.in>
2020-04-29 03:16:27 +0530

    Revert "Login Authguard Interface"
    This reverts commit 7e2ede5c5c5ba0ab56157e5636b2df1e56244fc1.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,54 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MainComponent } from './main/main.component';
-import { BlankComponent } from './pages/blank/blank.component';
-import { LoginComponent } from './login/login.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { RegisterComponent } from './register/register.component';
-import { AuthGuard } from './utils/guards/auth.guard';
-import { NonAuthGuard } from './utils/guards/non-auth.guard';
 
-const routes: Routes = [
-    {
-        path: '',
-        component: MainComponent,
-        canActivate: [AuthGuard],
-        children: [
-            {
-                path: 'profile',
-                component: ProfileComponent,
-                canActivate: [AuthGuard]
-            },
-            {
-                path: 'blank',
-                component: BlankComponent,
-                canActivate: [AuthGuard]
-            },
-            {
-                path: '',
-                // component: DashboardComponent
-                loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule),
-                canActivate: [AuthGuard]
-            }
-        ]
-    },
-    {
-        path: 'login',
-        component: LoginComponent,
-        canActivate: [NonAuthGuard]
-    },
-    {
-        path: 'register',
-        component: RegisterComponent,
-        canActivate: [NonAuthGuard]
-    },
-    { path: '**', redirectTo: '' }
-];
+
+const routes: Routes = [];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, {
-        useHash: true,
-    })],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
