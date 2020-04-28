0202110cc2663f21946a7627f8177195a11c30ea
junaid-iacademia <mohdjunaidahmed.si20@iacademia.in>
2020-04-29 03:47:04 +0530

    Revert "Revert "Revert "Login Authguard Interface"""
    This reverts commit 47ddf7009a67a4375824af4997f13f9e98f97087.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,44 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
-import { LoginComponent } from './login/login.component';
-import { HeaderComponent } from './main/header/header.component';
-import { FooterComponent } from './main/footer/footer.component';
-import { MenuSidebarComponent } from './main/menu-sidebar/menu-sidebar.component';
-import { BlankComponent } from './pages/blank/blank.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RegisterComponent } from './register/register.component';
-import { MessagesDropdownMenuComponent } from './main/header/messages-dropdown-menu/messages-dropdown-menu.component';
-import { NotificationsDropdownMenuComponent } from './main/header/notifications-dropdown-menu/notifications-dropdown-menu.component';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './utils/guards/auth-interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainComponent,
-    LoginComponent,
-    HeaderComponent,
-    FooterComponent,
-    MenuSidebarComponent,
-    BlankComponent,
-    ProfileComponent,
-    RegisterComponent,
-    MessagesDropdownMenuComponent,
-    NotificationsDropdownMenuComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
+    AppRoutingModule
   ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
