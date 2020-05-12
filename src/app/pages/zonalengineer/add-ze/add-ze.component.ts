e061eb0b9630ff8b9279f4e8b7bf5910ce98d577
Ridhanya2206 <64462442+Ridhanya2206@users.noreply.github.com>
2020-05-12 20:34:29 +0530

    add-ze files

---
+++ src/app/pages/zonalengineer/add-ze/add-ze.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-ze',
+  templateUrl: './add-ze.component.html',
+  styleUrls: ['./add-ze.component.scss']
+})
+export class AddZeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
