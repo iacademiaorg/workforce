26fb002bbc6fb5ce1eb4d3bad3285f45e198f4d0
Ridhanya2206 <64462442+Ridhanya2206@users.noreply.github.com>
2020-05-12 19:07:43 +0530

    zonalengineer module

---
+++ src/app/pages/zonalengineer/zonalengineer.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ZonalengineerRoutingModule } from './zonalengineer-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ZonalengineerRoutingModule
+  ]
+})
+export class ZonalengineerModule { }
