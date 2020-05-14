f485ce72161196644bf0a41b388771611414a51a
Ridhanya2206 <64462442+Ridhanya2206@users.noreply.github.com>
2020-05-14 20:08:36 +0530

    ze routing update

--- src/app/pages/zonalengineer/zonalengineer-routing.module.ts
+++ src/app/pages/zonalengineer/zonalengineer-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddZeComponent } from './add-ze/add-ze.component'
+import { AuthGuard } from 'src/app/utils/guards/auth.guard'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'add-ze',
+    component: AddZeComponent,
+    data: { title: 'zonalengineer' },
+    canActivate: [AuthGuard],
+},
+{
+  path: '',
+  redirectTo: 'add-ze',
+  pathMatch: 'full'
+}
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ZonalengineerRoutingModule { }
+export class ZonalengineerRouterModule { }
