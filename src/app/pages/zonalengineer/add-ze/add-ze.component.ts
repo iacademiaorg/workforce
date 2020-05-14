ac473bdc6d23667922df5677f44685c3fe071623
Ridhanya2206 <64462442+Ridhanya2206@users.noreply.github.com>
2020-05-14 20:02:48 +0530

    add-ze .ts update

--- src/app/pages/zonalengineer/add-ze/add-ze.component.ts
+++ src/app/pages/zonalengineer/add-ze/add-ze.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-ze',
@@ -6,10 +7,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-ze.component.scss']
 })
 export class AddZeComponent implements OnInit {
+fruits: Array<string> = ["apple", "pear", "kiwi", "banana", "grape", "strawberry", "grapefruit", "melon", "mango", "plum"];
 
-  constructor() { }
+formGroup: FormGroup;
 
-  ngOnInit(): void {
-  }
+//nameFormGroup: FormGroup;
+//emailFormGroup: FormGroup;
+pattern = new RegExp(/^\d{5}(?:\d{2})?$/);
+
+
+
+steps = [
+    { label: 'Confirm your name', content: 'Last name, First name.' },
+    { label: 'Confirm your contact information', content: '123-456-7890' },
+    { label: 'Confirm your address', content: '1600 Amphitheater Pkwy MTV' },
+    { label: 'You are now done', content: 'Finished!' }
+];
+
+get formArray(): AbstractControl | null { return this.formGroup.get('formArray'); }
+
+public registerForm: FormGroup;
+
+constructor(
+    private renderer: Renderer2,
+    private _formBuilder: FormBuilder
+) { }
+
+ngOnInit() {
+    this.formGroup = this._formBuilder.group({
+        formArray: this._formBuilder.array([
+            this._formBuilder.group({
+                name : ['', Validators.required],
+                dob : ['', Validators.required],
+                age : [''],
+                mobile : ['',Validators.required]
+            }),
+            this._formBuilder.group({
+                emailFormCtrl: ['', Validators.email],
+                street: ['', Validators.required],
+                state: ['', Validators.required],
+                city: ['', Validators.required],
+                zip: ['', Validators.compose([Validators.required,Validators.pattern('[0-9]{6}')])]
+               
+                
+                
+            }),
+            this._formBuilder.group({
+                image:['',Validators.required]
+            }),
+        ])
+    });
+
+    /*this.registerForm = new FormGroup({
+        email: new FormControl(null, Validators.required),
+        password: new FormControl(null, Validators.required)
+        
+        
+    });*/
+}
 
 }
