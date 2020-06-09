59d78d595159573f40eba13e549dbbf2d166b15d
RohitSi20 <64515540+RohitSi20@users.noreply.github.com>
2020-06-09 19:12:04 +0530

    Update user.js

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+var bcrypt = require("bcrypt");
+var BCRYPT_SALT_ROUNDS = 12;
+
 
 const User = require("../models/user");
 
@@ -25,35 +28,45 @@ router.post("/signup", (req, res, next) => {
     wSecQue: req.body.wSecQue,
     wSecAns: req.body.wSecAns,
     wadhaar: req.body.wadhaar
-  });
+ });
+  bcrypt.hash(wpassword,BCRYPT_SALT_ROUNDS)
+  .then(function(hashedPassword)
+  {
+    return user.save(username,hashedPassword);
+  })
+  .then(function(){
+    res.send();
+  })
+  .catch(function(error){
+    console.log("Error saving user:");
+    console.log(error);
+    next();
+  })
   user.save(function(err, user) {
     if (err) return console.error(err);
     console.log(" saved to user collection.");
   });
-  res.send("<h1>saved to database</h1>");
 });
 
+router.post("/login",function(req,res){
+const username=req.body.username;
+const password=req.body.password;
+user.getUserByUsername(username)
+.then(function(user){
+  return bcrypt.compare(password,user.password);
+})
+.then(function(samePassword){
+  if(!samePassword){
+    res.status(403).send();
+  }
+  res.send();
+})
+.catch(function(error){
+  console.log("Error authenticating User:");
+  console.log(error);
+  next();
+});
 
-
-
-router.post("/login", function(req, res){
-
-  const username = req.body.username;
-  const password = req.body.password;
-
-  User.findOne({wemail:username}, function(err, foundUser){
-    if(err){
-      res.send("<h1>got clicked</h1>");
-      console.log(err);
-    }else{
-      if(foundUser){
-        if(foundUser.wpassword === password){
-          res.send("logged in");
-        }
-      }
-    }
-
-  });
 });
 
 
