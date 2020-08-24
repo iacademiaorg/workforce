5d8bc574b465ff295650a04b166fa7918bbb2883
srishti-si20 <srishti.si20@iacademia.in>
2020-08-24 22:05:29 +0530

    Added google sign in feature

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit, OnDestroy, Renderer2 } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild, ElementRef } from '@angular/core';
 import { AppService } from '../utils/services/app.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../utils/services/auth.service';
+import { Router } from '@angular/router';
+import { StorageService } from '../utils/services/storage.service';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit{
+
     public loginForm: FormGroup;
+
+    auth2: any;
+    @ViewChild('loginRef', {static: true }) loginElement: ElementRef;
+
     constructor(
         private renderer: Renderer2,
         private appService: AppService,
-        private authService: AuthService
+        private authService: AuthService,
+        private router: Router,
+        private storage: StorageService
     ) { }
 
     ngOnInit() {
@@ -24,6 +33,8 @@ export class LoginComponent implements OnInit {
             email: new FormControl(null, Validators.required),
             password: new FormControl(null, Validators.required)
         });
+
+        this.googleSDK();
     }
 
     logIn() {
@@ -35,6 +46,58 @@ export class LoginComponent implements OnInit {
         } else {
             //   this.toastr.error('Hello world!', 'Toastr fun!');
         }
+    }
+
+   async prepareLoginButton() 
+    {
+ 
+        this.auth2.attachClickHandler(this.loginElement.nativeElement, {},
+          (googleUser) => {
+       
+            let profile = googleUser.getBasicProfile();
+            console.log('Token || ' + googleUser.getAuthResponse().id_token);
+            console.log('ID: ' + profile.getId());
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail());
+            
+            //code for initiating a session
+           
+            var id_token = googleUser.getAuthResponse().id_token;
+
+            this.storage.saveToken(id_token);
+            this.storage.saveUserID(profile.getId());
+            this.storage.saveUsername(profile.getName());
+            this.storage.saveUserID(profile.getId());
+            this.storage.saveLogoUrl(profile.getImageUrl());
+            this.router.navigate([''])
+          }, (error) => {
+            alert(JSON.stringify(error, undefined, 2));
+          });
+          
+       
+    }
+
+    async googleSDK() 
+    {
+        window['googleSDKLoaded'] = () => {
+          window['gapi'].load('auth2', () => {
+            this.auth2 = window['gapi'].auth2.init({
+              client_id: '582105515025-7t5ruhj9fplhu77gk5qpihioirmehi5m.apps.googleusercontent.com',
+              cookie_policy: 'single_host_origin',
+              scope: 'profile email'
+            });
+            this.prepareLoginButton();
+                            });
+                                         }   
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://apis.google.com/js/platform.js?onload=googleSDKLoaded";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'google-jssdk'));
+
     }
 
 }
